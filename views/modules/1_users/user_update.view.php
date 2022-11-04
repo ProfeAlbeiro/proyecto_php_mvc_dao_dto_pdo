8da13c9beb3dfe1edbaa0817b3cbe7f679e95715
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-11-03 21:39:13 -0500

    Subiendo el Proyecto

---
+++ views/modules/1_users/user_update.view.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Información</title>
+    <link rel="shortcut icon" href="img/css.png">
+    <link rel="stylesheet" href="../../css/all.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/styles-dashboard-bootstrap4.css">
+</head>
+
+<body>
+    <!-- Contenedor -->
+    <div class="container-fluid captura-id">
+        <!-- Migas de Pan -->
+        <div class="migas row d-flex align-items-center bg-white border-bottom">
+            <div class="col p-0">
+                <div aria-label="breadcrumb">
+                    <ol class="breadcrumb rounded-0 m-0 p-2 bg-white">
+                        <li class="breadcrumb-item"><a href="../0_mains/admin_main.html">Inicio</a></li>
+                        <li class="breadcrumb-item">Módulo Usuarios</li>
+                        <li class="breadcrumb-item active" aria-current="page">Acutalizar Usuario</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <!-- Título -->
+        <div class="titulo-contenido row">
+            <div class="col p-2 border-bottom d-flex justify-content-center align-items-center">
+                <div class="col-6 p-0 d-flex justify-content-start align-items-center">
+                    <h5 class="m-0">Actualizar Usuario</h5>
+                </div>
+                <div class="col-6 d-flex justify-content-end align-items-center p-0">
+                    <a href="user_read.html" class="btn btn-light">Consultar Usuarios</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Contenido -->
+        <div class="contenido row bg-light p-2">
+            <div class="col p-0 bg-light">
+                <form id="formUserCreate" name="formUserUpdate"
+                    class="card p-3 bg-dark text-white d-lg-flex justify-content-center w-100 border rounded p-2"
+                    action="user_read.html" method="post">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="perfil_usuario">Perfil</label>
+                            <select class="form-control class-perfil" id="perfil">
+                                <!-- <option></option> -->
+                                <option>usuario</option>
+                                <option>cliente</option>
+                                <option>empleado</option>
+                                <option>administrador</option>
+                            </select>
+                        </div>
+                        <div id="foto_group" class="form-group col-md-6 ocultar-control">
+                            <label for="foto">Foto</label>
+                            <input type="file" class="form-control p-1" id="foto" placeholder="Apellidos">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="nombres">Nombres</label>
+                            <input type="text" class="form-control" id="nombres" placeholder="Nombres">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="apellidos">Apellidos</label>
+                            <input type="text" class="form-control" id="apellidos" placeholder="Apellidos">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="correo">Correo</label>
+                            <input type="email" class="form-control" id="correo" placeholder="usuario@correo.com">
+                        </div>
+                        <div id="doc_identidad_group" class="form-group col-md-6 ocultar-control">
+                            <label for="doc_identidad">Documento de Identidad</label>
+                            <input type="text" class="form-control" id="doc_identidad" placeholder="123456789">
+                        </div>
+                        <div id="contrasena_us_group" class="form-group col-md-6 ocultar-control">
+                            <label for="contrasena_us">Contraseña</label>
+                            <input type="password" class="form-control" id="contrasena_us" placeholder="Entre 5 y 8 caracteres">
+                        </div>
+                        <div id="confirmacion_group" class="form-group col-md-6 ocultar-control">
+                            <label for="confirmacion">Confirmación</label>
+                            <input type="password" class="form-control" id="confirmacion" placeholder="Confirmar contraseña">
+                        </div>
+                        <div id="fechaNac_group" class="form-group col-md-6 ocultar-control">
+                            <label for="fechaNac">Fecha Nacimiento</label>
+                            <input type="date" class="form-control" id="fechaNac" placeholder="Nombres">
+                        </div>
+                        <div id="estado_group" class="form-group col-md-6 ocultar-control">
+                            <label for="estado">Estado</label>
+                            <select class="form-control" id="estado">
+                                <option>activo</option>
+                                <option>inactivo</option>
+                            </select>
+                        </div>
+                        <div id="salario_group" class="form-group col-md-6 ocultar-control">
+                            <label for="salario">Salario</label>
+                            <input type="number" class="form-control text-right" id="salario" placeholder="salario"
+                                value="1300000">
+                        </div>
+                    </div>
+                    <input type="submit" id="submit-user-create" class="btn btn-info mb-2" value="Actualizar">
+                    <button type="button" id="submit-user-create-cancel" class="btn btn-secondary" data-dismiss="modal"
+                        id="cerrar">Cerrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="../../js/sweetalert.min.js"></script>
+    <script src="../../js/jquery.slim.min.js"></script>
+    <script src="../../js/popper.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/scripts-dash.js"></script>
+</body>
+
+</html>
