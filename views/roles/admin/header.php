ae885ed512dd6fc79b8df0a022168c26e76e402e
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-11-12 04:00:07 -0500

    prueba

--- views/roles/admin/header.php
+++ views/roles/admin/header.php
@@ -118,7 +118,7 @@
 									<div class="card">
 										<ul class="list-group list-group-flush">
 											<li class="list-group-item p-0 bg-light">
-												<a href="?c=Users&a=config" class="card-link d-block p-2 px-lg-3 py-lg-1 ocul-panel">Configuración</a>
+												<a href="?c=Users&a=config" class="card-link d-block p-2 px-lg-3 py-lg-1 ocul-panel">Configuración Usuario</a>
 											</li>
 											<li class="list-group-item p-0 bg-light">
 												<a href="?c=Users&a=create" class="card-link d-block p-2 px-lg-3 py-lg-1 ocul-panel">Crear Usuario</a>
